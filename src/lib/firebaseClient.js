aa1ad392f7c7f7936618faf361e2637aa63fc13c
bernardyuzhe9 <97150285+bernardyuzhe9@users.noreply.github.com>
2025-10-14 23:20:15 +0800

    chore: update .gitignore and refactor Firebase configuration to use environment variables

--- src/lib/firebaseClient.js
+++ src/lib/firebaseClient.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCI5sTOi6L0p5KmSznFMpFBq_1fYjTk7Qk",
-  authDomain: "final-year-project-475109.firebaseapp.com",
-  projectId: "final-year-project-475109",
-  storageBucket: "final-year-project-475109.firebasestorage.app",
-  messagingSenderId: "924270264441",
-  appId: "1:924270264441:web:8833ccd749c241128d54a8",
-  measurementId: "G-84KVL1ZXBQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
